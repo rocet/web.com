5c9a2630de36f0d8ab37a9c9ed2fdf5a95b102f7
rocet <rocet@live.cn>
2014-10-11 14:22:29 +0800

    dsds

--- app/config/database.php
+++ app/config/database.php
@@ -57,7 +57,7 @@ return array(
 			'host'      => '127.0.0.1',
 			'database'  => 'web.com',
 			'username'  => 'root',
-			'password'  => '',
+			'password'  => '123456',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_general_ci',
 			'prefix'    => 'web_',
